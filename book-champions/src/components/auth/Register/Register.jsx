0a16d5ee2cf4f406da97e12ced32dd7439cd6fe4
Melina Bueno <melinabueno123@gmail.com>
2025-05-03 22:24:15 -0300

    desafio-register

--- book-champions/src/components/auth/Register/Register.jsx
+++ book-champions/src/components/auth/Register/Register.jsx
@@ -1,27 +1,77 @@
 import { Button, Col, Form, FormGroup, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthContainer from "../../AuthContainer/AuthContainer";
+import { useState } from "react";
+import { successToast } from "../../../utils/notifications";
 
 const Register = () => {
+
+    const navigate = useNavigate();
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChangeName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+        if (!name || !email || password.length < 7) {
+            alert("Todos los campos son obligatorios y la contraseña debe tener al menos 7 caracteres.");
+            return;
+        }
+        
+        fetch("http://localhost:3000/register", {
+            headers: {
+                "Content-type": "application/json"
+            },
+            method: "POST",
+            body: JSON.stringify({name, email, password})
+        })
+        .then(res => res.json())
+        .then(()=>{
+            successToast("¡Usuario creado exitosamente!")
+            navigate("/login");
+        })
+        .catch(err => console.log(err))
+    }
+
   return (
     <AuthContainer>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormGroup className="mb-3">
           <Form.Control
             type="text"
             placeholder="Nombre de usuario"
+            value={name}
+            onChange={handleChangeName}
           />
         </FormGroup>
         <FormGroup className="mb-3">
           <Form.Control
             type="email"
             placeholder="Correo electrónico"
+            value={email}
+            onChange={handleChangeEmail}
           />
         </FormGroup>
         <FormGroup className="mb-4">
           <Form.Control
             type="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={handleChangePassword}
           />
         </FormGroup>
         <Row>
