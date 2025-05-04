7dfd8c8afcc4d952a40e4edcb26e8ca53df20b32
Melina Bueno <melinabueno123@gmail.com>
2025-05-03 21:31:25 -0300

    loginUser

--- book-champions-api/src/services/auth.services.js
+++ book-champions-api/src/services/auth.services.js
@@ -27,3 +27,30 @@ export const registerUser = async (req,res) => {
 
     res.json(newUser.id);
 }
+
+export const loginUser = async (req, res) => {
+
+    const {email, password} = req.body;
+
+    const user = await User.findOne({
+        where: {
+            email
+        }
+    });
+
+    if(!user)
+        return res.status(401).send({message: "Usuario no existente"});
+
+    const comparison = await bcrypt.compare(password, user.password);
+
+    if(!comparison)
+        return res.status(401).send({ message: "Email y/o contraseña incorrecta"});
+
+    // Generate Token
+
+    const secretKey = "programacion3-2025"
+    const token = jwt.sign({email}, secretKey, {expiresIn: '1h'});
+
+    return res.json(token);
+
+}
